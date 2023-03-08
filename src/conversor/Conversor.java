1f618a4fa37ad06f90549d69ed8fa9f6caedcbc3
Juan JB <jubergo13@gmail.com>
2023-03-08 18:56:33 -0300

    iniciando el proyecto

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,31 @@
+package conversor;
+
+/**
+ *
+ * @author Gomez Juan
+ */
+public class Conversor {
+
+//Requisitos:
+//- El convertidor de moneda debe:
+//
+//       - Convertir de la moneda de tu país a Dólar
+//       - Convertir de la moneda de tu país  a Euros
+//       - Convertir de la moneda de tu país  a Libras Esterlinas
+//       - Convertir de la moneda de tu país  a Yen Japonés
+//       - Convertir de la moneda de tu país  a Won sul-coreano               
+//Recordando que también debe ser posible convertir inversamente, es decir:
+//       - Convertir de Dólar a la moneda de tu país
+//       - Convertir de Euros a la moneda de tu país
+//       - Convertir de Libras Esterlinas a la moneda de tu país
+//       - Convertir de Yen Japonés a la moneda de tu país
+//       - Convertir de Won sul-coreano a la moneda de tu país
+//      
+    
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
